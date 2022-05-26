238f2bfb2657eebfcf7e784c08dbc480b414e678
syntheticmagus <33846034+syntheticmagus@users.noreply.github.com>
2022-05-26 12:22:50 -0700

    Tweaks for first GitHub Pages demo.

--- test_package/src/index.js
+++ test_package/src/index.js
@@ -4,7 +4,7 @@ let assetsHostUrl;
 if (DEV_BUILD) {
     assetsHostUrl = "http://127.0.0.1:8181";
 } else {
-    assetsHostUrl = "https://nonlocal-assets-host-url";
+    assetsHostUrl = "https://syntheticmagus.github.io/metaverse-acclimation-guide-assets";
 }
 
 const ammoScript = document.createElement("script");
@@ -17,7 +17,7 @@ document.body.style.margin = "0";
 document.body.style.padding = "0";
 
 const title = document.createElement("p");
-title.innerText = "Babylon.js NPM Package Template";
+title.innerText = "Metaverse Acclimation Guide Demo Page";
 title.style.fontSize = "32pt";
 title.style.textAlign = "center";
 document.body.appendChild(title);
